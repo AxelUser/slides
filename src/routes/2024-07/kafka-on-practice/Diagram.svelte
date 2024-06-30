56f89a346ef2b6e3d84525828006296501080a5c
Alexey Maltsev <axel.user.perm@gmail.com>
2024-06-30 18:29:48 +0200

    WIP

---
+++ src/routes/2024-07/kafka-on-practice/Diagram.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let code: string;
+</script>
+
+<div class="mermaid flex flex-col items-center">
+	{`
+        %%{init: {'theme': 'dark', 'themeVariables': { 'darkMode': true }}}%%
+    `}
+	{code}
+</div>
